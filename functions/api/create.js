7730f38b78ba9c4fbce9cfa65c2d44fdbcf4ddf7
Okienak <cidmomo19@gmail.com>
2026-03-05 19:42:36 +0700

    Update create.js

--- functions/api/create.js
+++ functions/api/create.js
@@ -1,25 +1,22 @@
 export async function onRequestPost(context) {
     const { request, env } = context;
-
     try {
         const body = await request.json();
         
-        // BAGIAN CEK PASSWORD SUDAH DIHAPUS
-
-        // Generate ID Acak
+        // Buat ID Acak
         const randomId = Math.random().toString(36).substring(2, 8);
 
+        // Hanya simpan data server video
         const videoData = {
-            servers: body.servers,
-            downloadUrl: body.downloadUrl
+            servers: body.servers
         };
 
         await env.VIDEO_KV.put(`vid_${randomId}`, JSON.stringify(videoData));
 
-        return new Response(JSON.stringify({ success: true, id: randomId }), {
-            headers: { "Content-Type": "application/json" }
+        return new Response(JSON.stringify({ success: true, id: randomId }), { 
+            headers: { "Content-Type": "application/json" } 
         });
     } catch (err) {
-        return new Response(JSON.stringify({ error: "Server Error" }), { status: 500 });
+        return new Response(JSON.stringify({ error: "Gagal membuat data" }), { status: 500 });
     }
 }
