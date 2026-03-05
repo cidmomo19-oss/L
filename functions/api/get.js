dac87d042c4b806b25f2ac3c1e9b1caa20638dbd
Okienak <cidmomo19@gmail.com>
2026-03-06 03:33:35 +0700

    Update get.js

--- functions/api/get.js
+++ functions/api/get.js
@@ -2,20 +2,14 @@ export async function onRequestGet(context) {
     const { request, env } = context;
     const url = new URL(request.url);
     const id = url.searchParams.get("id");
+
     if (!id) return new Response(JSON.stringify({ error: "No ID" }), { status: 400 });
 
-    const cache = caches.default;
-    const cacheKey = new Request(url.toString(), request);
-    let response = await cache.match(cacheKey);
+    const targetUrl = await env.VIDEO_KV.get(`taptap_${id}`);
 
-    if (!response) {
-        const data = await env.VIDEO_KV.get(`vid_${id}`);
-        if (!data) return new Response(JSON.stringify({ error: "Not Found" }), { status: 404 });
+    if (!targetUrl) return new Response(JSON.stringify({ error: "Link Expired" }), { status: 404 });
 
-        response = new Response(data, {
-            headers: { "Content-Type": "application/json", "Cache-Control": "public, max-age=2592000, s-maxage=2592000", "Access-Control-Allow-Origin": "*" }
-        });
-        context.waitUntil(cache.put(cacheKey, response.clone()));
-    }
-    return response;
+    return new Response(JSON.stringify({ url: targetUrl }), {
+        headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" }
+    });
 }
